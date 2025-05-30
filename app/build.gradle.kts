9909ae6adbca0f48a2593d01b93eb85803b24536
VOsadchii <909404@gmail.com>
2025-05-30 19:26:08 +0300

    Setup checkstyle and fix violations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.51.0" // Актуальная версия на май 2025
     id("application")
     id("org.sonarqube") version "6.2.0.5505"
+    checkstyle
 }
 
 group = "hexlet.code"
