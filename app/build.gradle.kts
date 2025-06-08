ef4528fcdfc9f5fd9e42645880c44a8dec52d23c
VOsadchii <909404@gmail.com>
2025-06-08 19:39:05 +0300

    Excluded java plugin from gradle config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     id("com.github.ben-manes.versions") version "0.51.0" // Актуальная версия на май 2025
     id("application")
     id("org.sonarqube") version "6.2.0.5505"
@@ -11,11 +10,6 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
